9a4961186b4bef4f6f00b5193356be9d56bc6155
Tonyjlum <tonyjlum@gmail.com>
2018-12-23 17:58:51 -0500

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+puts "Destroy all user"
+
+User.create(email: "tonyjlum@gmail.com", password: "greentea", age: 29, alias: "Mumbles")
